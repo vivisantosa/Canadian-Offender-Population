f7665eff6791fa2e56b50b4e60d81eddf56c9b24
Momotaz-Mahin-Khan <momotazmahinkhan@gmail.com>
2020-09-28 00:01:42 -0400

    Chart 2

---
+++ chart 2 code/plots.js
@@ -0,0 +1,260 @@
+
+
+//1.  extract the data from csv
+//1.a load csv data
+var table4 = "../../01 Resources/Data/table4.csv" 
+// var table4 = "table4.csv" 
+
+
+// Important! The HTML is edited to have selDat1 and selDat2
+// Selecting elements on the HTML
+var selDat1 = d3.select("#selDataset1")
+var selDat2 = d3.select("#selDataset2")
+
+var provinceNames = ["National","BC","AB","SK","MB","ON","QC","NB","NL","PE","NS","NW","YU","NU"] 
+var statusList = ["In Custody", "Community"]
+var Year = ['2013', '2014', '2015', '2016', '2017', '2018' ]
+
+
+
+// Create dropdown
+provinceNames.forEach( d=> {
+  selDat1.append("option").text(d)
+  } )
+
+statusList.forEach( d=> {
+  selDat2.append("option").text(d)
+  } )
+
+
+// Set the province, status and Race as global variable. This way, we just have to change province and sex
+var Province = "National"
+var Status= "In Custody"
+
+// Load data into
+d3.csv(table4).then(function(d) {
+  console.log(d)
+  
+  // Initialize the function
+  function init() {
+
+    var trace1Y = [] 
+    var trace2Y = []
+    var trace3Y = []
+    var trace4Y = []
+    
+    // push data to trace 1 or 2 only if it matches the condition
+    d.forEach(function(data) {
+
+      // console.log(data.Percent_Population)
+      if ( data.province == Province && data.in_custody_community == Status && data.race == "Black" ){
+        trace1Y.push(data.avg_length)
+      }
+      if ( data.province == Province && data.in_custody_community == Status && data.race == "Aboriginal" ){
+        trace2Y.push(data.avg_length)
+      }
+      if ( data.province == Province && data.in_custody_community == Status && data.race == "White" ){
+        trace3Y.push(data.avg_length)
+      }
+      if ( data.province == Province && data.in_custody_community == Status && data.race == "Other Minority" ){
+        trace4Y.push(data.avg_length)
+        // console.log(trace1Y)
+      }
+    });
+
+    // set up trace to prepare data for one of the table
+    var trace1 = {
+      x: Year,
+      y: trace1Y,
+      name: 'Black',
+      type: 'scatter'
+    };
+    
+    // set up trace to prepare data for one of the table
+    var trace2 = {
+      x: Year,
+      y: trace2Y,
+      name: 'Aboriginal',
+      type: 'scatter'
+    };
+    // set up trace to prepare data for one of the table
+    var trace3 = {
+      x: Year,
+      y: trace3Y,
+      name: 'White',
+      type: 'scatter'
+    };
+    // set up trace to prepare data for one of the table
+    var trace4 = {
+      x: Year,
+      y: trace4Y,
+      name: 'Other Minority',
+      type: 'scatter'
+    };
+    
+    // draw the plots
+    var data = [trace1, trace2, trace3, trace4];
+    var layout = {showlegend: true};
+    var CHART = d3.selectAll("#plot").node();
+    Plotly.newPlot(CHART, data, layout);
+
+    
+
+
+};
+init()
+// TO DO! On change, run this program
+  // d3.selectAll("body").on("change", updatePlotly);
+  // d3.selectAll("body").on("change", updatePlotly);
+// Call the initialization function
+
+d3.select("#selDataset1").on("change", updateProvince);
+d3.select("#selDataset2").on("change", updateStatus);
+  
+// Create function to capture Sex selection
+  function updateStatus() {
+    var dropdownMenu2 = d3.select("#selDataset2");
+    var dataset2 = dropdownMenu2.node().value;
+    var Status = dataset2
+    console.log(Status)
+    
+    var trace1Y = [] 
+    var trace2Y = []
+    var trace3Y = []
+    var trace4Y = []
+    
+    // push data to trace 1 or 2 only if it matches the condition
+    d.forEach(function(data) {
+
+      // console.log(data.Percent_Population)
+      if ( data.province == Province && data.in_custody_community == Status && data.race == "Black" ){
+        trace1Y.push(data.avg_length)
+      }
+      if ( data.province == Province && data.in_custody_community == Status && data.race == "Aboriginal" ){
+        trace2Y.push(data.avg_length)
+      }
+      if ( data.province == Province && data.in_custody_community == Status && data.race == "White" ){
+        trace3Y.push(data.avg_length)
+      }
+      if ( data.province == Province && data.in_custody_community == Status && data.race == "Other Minority" ){
+        trace4Y.push(data.avg_length)
+        // console.log(trace1Y)
+      }
+
+    });
+
+    // set up trace to prepare data for one of the table
+    var trace1 = {
+      x: Year,
+      y: trace1Y,
+      name: 'Black',
+      type: 'scatter'
+    };
+    
+    // set up trace to prepare data for one of the table
+    var trace2 = {
+      x: Year,
+      y: trace2Y,
+      name: 'Aboriginal',
+      type: 'scatter'
+    };
+    // set up trace to prepare data for one of the table
+    var trace3 = {
+      x: Year,
+      y: trace3Y,
+      name: 'White',
+      type: 'scatter'
+    };
+    // set up trace to prepare data for one of the table
+    var trace4 = {
+      x: Year,
+      y: trace4Y,
+      name: 'Other Minority',
+      type: 'scatter'
+    };
+    
+    // draw the plots
+    var data = [trace1, trace2, trace3, trace4];
+     var layout = {showlegend: true};
+    var CHART = d3.selectAll("#plot").node();
+    
+    Plotly.newPlot(CHART, data, layout);
+    
+  }
+
+
+
+// Create function to capture Province selection, then call init()
+function updateProvince() {
+  var dropdownMenu1 = d3.select("#selDataset1");
+  var dataset = dropdownMenu1.node().value;
+  var Province = dataset
+  console.log(Province)
+  
+  var trace1Y = [] 
+  var trace2Y = []
+  var trace3Y = []
+  var trace4Y = []
+  
+  // push data to trace 1 or 2 only if it matches the condition
+  d.forEach(function(data) {
+
+    // console.log(data.Percent_Population)
+    if ( data.province == Province && data.in_custody_community == Status && data.race == "Black" ){
+      trace1Y.push(data.avg_length)
+    }
+    if ( data.province == Province && data.in_custody_community == Status && data.race == "Aboriginal" ){
+      trace2Y.push(data.avg_length)
+    }
+    if ( data.province == Province && data.in_custody_community == Status && data.race == "White" ){
+      trace3Y.push(data.avg_length)
+    }
+    if ( data.province == Province && data.in_custody_community == Status && data.race == "Other Minority" ){
+      trace4Y.push(data.avg_length)
+      // console.log(trace1Y)
+    }
+
+  });
+
+  // set up trace to prepare data for one of the table
+  var trace1 = {
+    x: Year,
+    y: trace1Y,
+    name: 'Black',
+    type: 'scatter'
+  };
+  
+  // set up trace to prepare data for one of the table
+  var trace2 = {
+    x: Year,
+    y: trace2Y,
+    name: 'Aboriginal',
+    type: 'scatter'
+  };
+  //  // set up trace to prepare data for one of the table
+  var trace3 = {
+    x: Year,
+    y: trace3Y,
+    name: 'White',
+    type: 'scatter'
+  };
+  //  // set up trace to prepare data for one of the table
+  var trace4 = {
+    x: Year,
+    y: trace4Y,
+    name: 'Other Minority',
+    type: 'scatter'
+  };
+  
+  // draw the plots
+  var data = [trace1, trace2, trace3, trace4];
+  var layout = {showlegend: true};
+  var CHART = d3.selectAll("#plot").node();
+  Plotly.newPlot(CHART, data, layout);
+  
+}
+
+})
+
+
+
