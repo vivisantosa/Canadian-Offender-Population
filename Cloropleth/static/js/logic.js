7b82e9fccf27ed32f07e93de487509adb8567ee5
vivisantosa <65078785+vivisantosa@users.noreply.github.com>
2020-09-29 21:03:24 -0400

    this is the replacement files

---
+++ Cloropleth/static/js/logic.js
@@ -0,0 +1,268 @@
+/* UofT SCS Data Analytics Boot Camp
+  Project - 2
+  Filename: logic.js
+  Author:   Vivianti Santosa
+  Date:     2020-09-29
+*/
+
+// Provinces data ------------------------------------------------------------
+var provinces = [
+  {
+    PRUID: 59,
+    Province: "BC",
+    Location: [54.0000, -125.0000],
+  },
+  {
+    PRUID: 24,
+    Province: "QC",
+    Location: [54.0000, -75.0000],
+  },
+  {
+    PRUID: 62,
+    Province: "NU",
+    Location: [62.0000, -98.0000],
+  },
+  {
+    PRUID: 11,
+    Province: "PE",
+    Location: [46.5000, -63.0000],
+  },
+  {
+    PRUID: 47,
+    Province: "SK",
+    Location: [54.0000, -107.0000],
+  },
+  {
+    PRUID: 60,
+    Province: "YU",
+    Location: [62.0000, -135.0000],
+  },
+  {
+    PRUID: 46,
+    Province: "MB",
+    Location: [54.0000, -99.0000],    
+  },
+  {
+    PRUID: 35,
+    Province: "ON",
+    Location: [54.0000, -90.0000],
+  },
+  {
+    PRUID: 13,
+    Province: "NB",
+    Location: [47.0000, -67.0000],
+  },
+  {
+    PRUID: 61,
+    Province: "NW",
+    Location: [62.0000, -115.0000],
+  },
+  {
+    PRUID: 48,
+    Province: "AB",
+    Location: [54.0000, -116.0000],
+  },
+  {
+    PRUID: 10,
+    Province: "NL",
+    Location: [54.0000, -62.0000],
+  },
+  {
+    PRUID: 12,
+    Province: "NS",
+    Location: [45.0000, -65.0000],
+  }
+];
+
+// Creating map object -------------------------------------------------------
+var myMap = L.map("map", {
+  center: [55.0000, -97.0000],
+  zoom: 4
+});
+
+// Adding tile layer
+// streetMap layers
+var streetMap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+  tileSize: 512,
+  maxZoom: 18,
+  zoomOffset: -1,
+  id: "mapbox/streets-v11",
+  accessToken: API_KEY
+});
+// lightMap layer
+var lightMap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "light-v10",
+  accessToken: API_KEY
+}).addTo(myMap);
+
+// Defining a baseMaps object that contains all of our different map choices. Only one
+// of these maps will be visible at a time!
+var baseMaps = {
+  //"Street Map": streetMap,
+  "Light Map": lightMap
+};
+
+// We create the layers for our two different sets of data, earthquakes and
+// tectonicplates.
+var aboriginRDI_layer = new L.LayerGroup();
+var blackRDI_layer = new L.LayerGroup();
+var Incarcerated_Pop_layer = new L.LayerGroup();
+
+// We define an object that contains all of our overlays. Any combination of
+// these overlays may be visible at the same time!
+var overlays = {
+  "Aborigin Disparity Index": aboriginRDI_layer,
+  "Black Disparity Index": blackRDI_layer,
+  // "Incarcerated Population": Incarcerated_Pop_layer
+};
+
+// Then we add a control to the map that will allow the user to change which
+// layers are visible.
+L.control
+  .layers(baseMaps, overlays)
+  .addTo(myMap);
+
+// Load in geojson data ------------------------------------------------------
+var geoData = "static/data/canada_provinces.geojson";
+
+// Grab data with d3
+d3.json(geoData, function(data) {
+
+  // Grab second data with d3  
+  var Data1 = "static/data/xixi3.json";
+  d3.json(Data1, function(data1) {
+
+    // Loop through the geoJSON data then for each object append "CRIME DATA" information
+    data.features.forEach((user) => {
+      // iterare through province
+      for (var i=0; i < 13; i++) {
+        if (data1[i].PRUID == user.properties.PRUID) {
+          // push each of the information to geoJson properties
+          user.properties["Location"] = data1[i].Location;
+          user.properties["Pct_Aborigin_Pop"] = data1[i].Pct_Aborigin_Pop;
+          user.properties["Pct_Aborigin_Arrested"] = data1[i].Pct_Aborigin_Arrested;
+          user.properties["Aborigin_Disparity_Index"] = data1[i].aboriginal_disparity_index;
+          user.properties["Black_Disparity_Index"] = data1[i].black_disparity_index;
+          user.properties["Other_Disparity_Index"] = data1[i].other_disparity_index;
+          user.properties["White_Disparity_Index"] = data1[i].white_disparity_index;
+          user.properties["Incarcerated_Pop"] = data1[i].Incarcerated_Pop;
+        }
+      }
+    });  
+    //--------------------------------------
+    console.log("here 3",data);
+
+    // Create choropleth layer for Aborigin_Disparity_Index
+    L.choropleth(data, {
+      // Set valueProperty (value that define the color) as: RDIvalue
+      valueProperty: "Aborigin_Disparity_Index",
+
+      scale: ["#ffffb2", "#b10026"],// Set color scale
+      steps: 5,// Number of breaks in step range
+      mode: "q",// q for quartile, e for equidistant, k for k-means
+      style: {
+        color: "#fff",// Border color
+        weight: 1,
+        fillOpacity: 0.8
+      },
+      // Binding a pop-up to each layer
+      onEachFeature: function(feature, layer) {
+        layer.bindPopup("Province/Territory: " + feature.properties.PRENAME + 
+          "<br>Race Disparity Index:" + feature.properties.Aborigin_Disparity_Index  +
+          "<br>Aborigin Population:" + feature.properties.Pct_Aborigin_Pop  +
+          "<br>Aborigin Inmates:" + feature.properties.Pct_Aborigin_Arrested );
+      }  
+      // create aboriginRDI_layer  
+      }).addTo(aboriginRDI_layer);
+      // Then we add the aboriginRDI_layer to our map for initial display.
+      aboriginRDI_layer.addTo(myMap);
+  
+    // Create choropleth layer for Black_Disparity_Index
+    L.choropleth(data, {
+      // Set valueProperty (value that define the color) as: RDIvalue
+      valueProperty: "Black_Disparity_Index",
+
+      scale: ["#ffffb2", "#b10026"],// Set color scale
+      steps: 8,// Number of breaks in step range
+      mode: "q",// q for quartile, e for equidistant, k for k-means
+      style: {
+        color: "#fff",// Border color
+        weight: 1,
+        fillOpacity: 0.8
+      },
+      // Binding a pop-up to each layer
+      onEachFeature: function(feature, layer) {
+        layer.bindPopup("Province/Territory: " + feature.properties.PRENAME + 
+          "<br>Race Disparity Index:" + feature.properties.Black_Disparity_Index);
+      }  
+      // create blackRDI_layer   
+      }).addTo(blackRDI_layer);
+  
+    // Create choropleth layer for Incarcerated_Population
+    L.choropleth(data, {
+      // Set valueProperty (value that define the color) as: RDIvalue
+      valueProperty: "Incarcerated_Pop",
+
+      scale: ["#e6e6ff", "#000080"],// Set color scale
+      steps: 5,// Number of breaks in step range
+      mode: "q",// q for quartile, e for equidistant, k for k-means
+      style: {
+        color: "#fff",// Border color
+        weight: 1,
+        fillOpacity: 0.8
+      },
+      // Binding a pop-up to each layer
+      onEachFeature: function(feature, layer) {
+        layer.bindPopup("Province/Territory: " + feature.properties.PRENAME + 
+          "<br>Incarcerated_Pop:" + feature.properties.Incarcerated_Pop);
+      }  
+      // Incarcerated_Pop_layer  
+      }).addTo(Incarcerated_Pop_layer);
+
+    // Create TEXT LAYER
+    for (var i = 0; i < 13; i++) {
+      L.marker(provinces[i].Location, {
+        icon: L.divIcon({
+            className: 'text-labels',   // Set class for CSS styling
+            html: provinces[i].Province
+        }),
+        zIndexOffset: 1000     // Make appear above other map features
+      }).addTo(myMap); 
+    }
+
+    // Set up LEGEND ------------------------------------------------------------------------------------
+    var legend = L.control({ position: "bottomleft" });
+    legend.onAdd = function() {
+      var div = L.DomUtil.create("div", "info legend");
+      var limits = [-2, 0, 2, 4, 6];
+      var colors = ["#ffffb2", "#ebbf8f", "#d87f6c", "#c43f49", "#b10026"];
+      var labels = [];
+
+      // Create legend info and Add min & max
+      var legendInfo = "<h4>Race Disparity Index</h4>" +
+        "<div class=\"labels\">" +
+          "<div class=\"min\">" + limits[0] + "</div>" +
+          "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+        "</div>";
+
+      div.innerHTML = legendInfo;
+
+      // Create block sample colors
+      limits.forEach(function(limit, index) {
+        labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
+      });
+
+      div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+      return div;
+    };
+
+    // Adding legend to the map
+    legend.addTo(myMap);
+  });
+});
+
+
+
---
+++ Cloropleth/static/js/plots.js
@@ -0,0 +1,193 @@
+
+//1.  extract the data from csv
+//1.a load csv data
+// var table1 = "../../01 Resources/Data/table1.csv"
+var table1 = "../Data/table1.json" 
+// var table1 = "table1.json" 
+
+
+
+// Important! The HTML is edited to have selDat1 and selDat2
+// Selecting elements on the HTML
+var selDat1 = d3.select("#selDataset1")
+var selDat2 = d3.select("#selDataset2")
+
+var provinceNames = ["National","BC","AB","SK","MB","ON","QC","NB","NL","PE","NS","NW","YU","NU"] 
+var sexList = ["Male", "Female", "Both"]
+var Races = ['Aboriginal', 'Black', 'Other Minority','White']
+
+
+
+// Create dropdown
+provinceNames.forEach( d=> {
+  selDat1.append("option").text(d)
+  } )
+
+sexList.forEach( d=> {
+  selDat2.append("option").text(d)
+  } )
+
+
+
+// Set the province and sex as global variable. This way, we just have to change province and sex
+var Province = "National"
+var Sex = "Both"
+
+// Load data into
+// d3.csv(table1).then(function(d) {
+d3.json(table1,function(d) {
+
+  
+  // Initialize the function
+  function init() {
+
+    var trace1Y = [] 
+    var trace2Y = []
+    
+    // push data to trace 1 or 2 only if it matches the condition
+    d.forEach(function(data) {
+
+      // console.log(data.Percent_Population)
+      if ( data.province == Province && data.sex == Sex ){
+        trace1Y.push(data.race_percent_arrested)
+        trace2Y.push(data.race_percent_population)
+
+        // console.log(trace1Y)
+      }
+
+    });
+
+    // set up trace to prepare data for one of the table
+    var trace1 = {
+      x: Races,
+      y: trace1Y,
+      name: 'Sum of Race % Arrested',
+      type: 'bar'
+    };
+    
+    // set up trace to prepare data for one of the table
+    var trace2 = {
+      x: Races,
+      y: trace2Y,
+      name: 'Sum of Race % Population',
+      type: 'bar'
+    };
+    
+    // draw the plots
+    var data = [trace1, trace2];
+    var layout = {barmode: 'group'};
+    var CHART = d3.selectAll("#plot").node();
+    Plotly.newPlot(CHART, data, layout);
+
+    
+  }
+init()
+
+// TO DO! On change, run this program
+  // d3.selectAll("body").on("change", updatePlotly);
+  // d3.selectAll("body").on("change", updatePlotly);
+// Call the initialization function
+
+d3.select("#selDataset1").on("change", updateProvince);
+d3.select("#selDataset2").on("change", updateSex);
+
+  
+// Create function to capture Sex selection
+  function updateSex() {
+    var dropdownMenu2 = d3.select("#selDataset2");
+    var dataset2 = dropdownMenu2.node().value;
+    var Sex = dataset2
+    console.log(Sex)
+    
+    var trace1Y = [] 
+    var trace2Y = []
+    
+    // push data to trace 1 or 2 only if it matches the condition
+    d.forEach(function(data) {
+
+      // console.log(data.Percent_Population)
+      if ( data.province == Province && data.sex == Sex ){
+        trace1Y.push(data.race_percent_arrested)
+        trace2Y.push(data.race_percent_population)
+
+        // console.log(trace1Y)
+      }
+
+    });
+
+    // set up trace to prepare data for one of the table
+    var trace1 = {
+      x: Races,
+      y: trace1Y,
+      name: 'Sum of Race % Arrested',
+      type: 'bar'
+    };
+    
+    // set up trace to prepare data for one of the table
+    var trace2 = {
+      x: Races,
+      y: trace2Y,
+      name: 'Sum of Race % Population',
+      type: 'bar'
+    };
+    
+    // draw the plots
+    var data = [trace1, trace2];
+    var layout = {barmode: 'group'};
+    var CHART = d3.selectAll("#plot").node();
+    
+    Plotly.newPlot(CHART, data, layout);
+    
+  }
+
+
+
+// Create function to capture Province selection, then call init()
+function updateProvince() {
+  var dropdownMenu1 = d3.select("#selDataset1");
+  var dataset = dropdownMenu1.node().value;
+  var Province = dataset
+  console.log(Province)
+  
+  var trace1Y = [] 
+  var trace2Y = []
+  
+  // push data to trace 1 or 2 only if it matches the condition
+  d.forEach(function(data) {
+
+    // console.log(data.Percent_Population)
+    if ( data.province == Province && data.sex == Sex ){
+      trace1Y.push(data.race_percent_arrested)
+      trace2Y.push(data.race_percent_population)
+
+      // console.log(trace1Y)
+    }
+
+  });
+
+  // set up trace to prepare data for one of the table
+  var trace1 = {
+    x: Races,
+    y: trace1Y,
+    name: 'Sum of Race % Arrested',
+    type: 'bar'
+  };
+  
+  // set up trace to prepare data for one of the table
+  var trace2 = {
+    x: Races,
+    y: trace2Y,
+    name: 'Sum of Race % Population',
+    type: 'bar'
+  };
+  
+  // draw the plots
+  var data = [trace1, trace2];
+  var layout = {barmode: 'group'};
+  var CHART = d3.selectAll("#plot").node();
+  Plotly.newPlot(CHART, data, layout);
+  
+}
+
+})
+
